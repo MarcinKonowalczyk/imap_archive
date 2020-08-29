f68fdb765b068c9330a7aa0ed96c49657bc760ff
Marcin Konowalczyk <marcin.konowalczyk@chem.ox.ac.uk>
2020-08-29 16:58:12 +0100

    moved body of archiver to: if __name__=="__main__"

--- imap_email_archiver_improved.py
+++ imap_email_archiver_improved.py
@@ -51,44 +51,44 @@ def make_folder_if_absent(path, folder_name):
         os.mkdir(full_path)
     return full_path
 
+if __name__ == "__main__":
 
-SAVE_LOCATION = "/Users/Andrew/Desktop/Oxford_emails/"
-SAVE_FILES = True
-FOLDER_LIMIT = None
+    SAVE_LOCATION = "./emails/"
+    SAVE_FILES = True
+    FOLDER_LIMIT = None
 
-with open("credentials.json", "r") as read_file:
-    credentials = json.load(read_file)
+    with open("credentials.json", "r") as read_file:
+        credentials = json.load(read_file)
 
-username = credentials["username"]
-password = credentials["password"]
-server = credentials["server_name"]
+    username = credentials["username"]
+    password = credentials["password"]
+    server = credentials["server_name"]
 
-with imap_tools.MailBox(server).login(username, password) as mailbox:
-    for folder in mailbox.folder.list():
-        mailbox_name = folder["name"]
-        mailbox.folder.set(mailbox_name)
-        mailbox_folder = make_folder_if_absent(SAVE_LOCATION, sanitize_filename(mailbox_name))
-        for i, msg in enumerate(mailbox.fetch(reverse=False, mark_seen=False, limit=FOLDER_LIMIT)):
+    with imap_tools.MailBox(server).login(username, password) as mailbox:
+        for folder in mailbox.folder.list():
+            mailbox_name = folder["name"]
+            mailbox.folder.set(mailbox_name)
+            mailbox_folder = make_folder_if_absent(SAVE_LOCATION, sanitize_filename(mailbox_name))
+            for i, msg in enumerate(mailbox.fetch(reverse=False, mark_seen=False, limit=FOLDER_LIMIT)):
 
-            if SAVE_FILES:
-                # Make folder for email thread with same name as msg.subject
-                sanitized_subject = sanitize_filename(msg.subject)
-                if sanitized_subject == '':
-                    sanitized_subject = enumerate_file_path('No subject')
-                subject_folder = make_folder_if_absent(mailbox_folder, sanitized_subject)
+                if SAVE_FILES:
+                    # Make folder for email thread with same name as msg.subject
+                    sanitized_subject = sanitize_filename(msg.subject)
+                    if sanitized_subject == '':
+                        sanitized_subject = enumerate_file_path('No subject')
+                    subject_folder = make_folder_if_absent(mailbox_folder, sanitized_subject)
 
-                json_filename = enumerate_file_path(os.path.join(subject_folder, 'message.json'))
-                encoded_message = email_to_json.json_encode(msg)
-                write_to_file(json_filename, encoded_message, as_bytes=False)
+                    json_filename = enumerate_file_path(os.path.join(subject_folder, 'message.json'))
+                    encoded_message = email_to_json.json_encode(msg)
+                    write_to_file(json_filename, encoded_message, as_bytes=False)
 
-                # To decode json representation of imap_tools.message.MailMessage object:
-                # b = email_to_json.json_decode(a)
+                    # To decode json representation of imap_tools.message.MailMessage object:
+                    # b = email_to_json.json_decode(a)
 
-                write_out_html(msg.subject, subject_folder, msg.html)
-                for att in msg.attachments:
-                    file_path = enumerate_file_path(os.path.join(subject_folder, att.filename))
-                    write_to_file(file_path, att.payload, as_bytes=True)
+                    write_out_html(msg.subject, subject_folder, msg.html)
+                    for att in msg.attachments:
+                        file_path = enumerate_file_path(os.path.join(subject_folder, att.filename))
+                        write_to_file(file_path, att.payload, as_bytes=True)
 
-            print('\r' * len(str(i - 1)), f'{mailbox_name}: downloaded {i} message{plural(i)}...', end="",
-                  flush=True)
-2
+                print('\r' * len(str(i - 1)),
+                    f'{mailbox_name}: downloaded {i} message{plural(i)}...', end='',flush=True)
