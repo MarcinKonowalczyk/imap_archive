76689160bb253c6d3b5480b2c7aaad70d9a05d93
Marcin Konowalczyk <marcin.konowalczyk@chem.ox.ac.uk>
2020-08-29 18:44:22 +0100

    Changed the progress bar

--- imap_email_archiver_improved.py
+++ imap_email_archiver_improved.py
@@ -1,28 +1,18 @@
-import imap_tools
 import json
-from pathvalidate import sanitize_filename
 import os
+from shutil import rmtree
+from functools import partial
+
+import imap_tools
+from pathvalidate import sanitize_filename
+from progressbar import ProgressBar # progressbar2 library
+
 import email_to_json
 
-
-def plural(num):
-    try:
-        if num > 1:
-            return 's'
-        else:
-            return ''
-    except TypeError:
-        pass
-
-
 def write_to_file(filename, payload, as_bytes):
-    if as_bytes:
-        method = 'wb'
-    else:
-        method = 'w'
+    method = 'wb' if as_bytes else 'w'
     with open(filename, method) as write_file:
         write_file.write(payload)
-
 
 def enumerate_file_path(path):
     if os.path.exists(path):
@@ -42,11 +32,11 @@ def write_out_html(subject, folder_name, body):
     subject = sanitize_filename(subject)
     filename = f"{subject[:50]}.html"
     file_path = enumerate_file_path(os.path.join(folder_name, filename))
-    open(file_path, "w").write(body)
-
+    open(file_path, "w").write(body).close()
 
 def make_folder_if_absent(path, folder_name):
     full_path = os.path.join(path, folder_name)
+    
     if not os.path.isdir(full_path):
         os.mkdir(full_path)
     return full_path
@@ -54,6 +44,10 @@ def make_folder_if_absent(path, folder_name):
 if __name__ == "__main__":
 
     SAVE_LOCATION = "./emails/"
+    try: rmtree(SAVE_LOCATION);
+    except: pass
+    make_folder_if_absent('.',SAVE_LOCATION)
+
     SAVE_FILES = True
     FOLDER_LIMIT = None
 
@@ -66,29 +60,37 @@ if __name__ == "__main__":
 
     with imap_tools.MailBox(server).login(username, password) as mailbox:
         for folder in mailbox.folder.list():
-            mailbox_name = folder["name"]
-            mailbox.folder.set(mailbox_name)
-            mailbox_folder = make_folder_if_absent(SAVE_LOCATION, sanitize_filename(mailbox_name))
-            for i, msg in enumerate(mailbox.fetch(reverse=False, mark_seen=False, limit=FOLDER_LIMIT)):
+            folder_name = folder["name"]
+            mailbox.folder.set(folder_name)
+            fetch = partial(mailbox.fetch,reverse=False, mark_seen=False, limit=FOLDER_LIMIT)
 
-                if SAVE_FILES:
-                    # Make folder for email thread with same name as msg.subject
-                    sanitized_subject = sanitize_filename(msg.subject)
-                    if sanitized_subject == '':
-                        sanitized_subject = enumerate_file_path('No subject')
-                    subject_folder = make_folder_if_absent(mailbox_folder, sanitized_subject)
+            headers = [h for h in fetch(headers_only=True)]
+            if SAVE_FILES:
+                if len(headers)>0:
+                    with ProgressBar(max_value=len(headers)-1,prefix=f"{folder_name}:") as bar:
+                        for i, msg in enumerate(fetch()):
+                            # Make folder for the mailbox
+                            mailbox_folder = make_folder_if_absent(SAVE_LOCATION,sanitize_filename(folder_name))
+                            
+                            # Make folder for email thread with same name as msg.subject
+                            sanitized_subject = sanitize_filename(msg.subject)
+                            if not sanitized_subject:
+                                sanitized_subject = enumerate_file_path('No subject')
+                            subject_folder = make_folder_if_absent(mailbox_folder, sanitized_subject)
 
-                    json_filename = enumerate_file_path(os.path.join(subject_folder, 'message.json'))
-                    encoded_message = email_to_json.json_encode(msg)
-                    write_to_file(json_filename, encoded_message, as_bytes=False)
+                            json_filename = enumerate_file_path(os.path.join(subject_folder, 'message.json'))
+                            encoded_message = email_to_json.json_encode(msg)
+                            write_to_file(json_filename, encoded_message, as_bytes=False)
 
-                    # To decode json representation of imap_tools.message.MailMessage object:
-                    # b = email_to_json.json_decode(a)
+                            # To decode json representation of imap_tools.message.MailMessage object:
+                            # b = email_to_json.json_decode(a)
 
-                    write_out_html(msg.subject, subject_folder, msg.html)
-                    for att in msg.attachments:
-                        file_path = enumerate_file_path(os.path.join(subject_folder, att.filename))
-                        write_to_file(file_path, att.payload, as_bytes=True)
-
-                print('\r' * len(str(i - 1)),
-                    f'{mailbox_name}: downloaded {i} message{plural(i)}...', end='',flush=True)
+                            write_out_html(msg.subject, subject_folder, msg.html)
+                            for att in msg.attachments:
+                                file_path = enumerate_file_path(os.path.join(subject_folder, att.filename))
+                                write_to_file(file_path, att.payload, as_bytes=True)
+                            bar.update(i)
+            else:
+                for msg in headers:
+                    print(msg.subject)
+    print('DONE')
